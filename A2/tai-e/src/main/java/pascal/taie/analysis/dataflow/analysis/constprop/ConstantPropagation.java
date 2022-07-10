e31c7d4e3d24cbdadcf1787d3bb5f9cc398b7d68
daohwang <1760730301@qq.com>
2022-07-11 01:24:05 +0800

    partially finish ConstantPropagation

--- A2/tai-e/src/main/java/pascal/taie/analysis/dataflow/analysis/constprop/ConstantPropagation.java
+++ A2/tai-e/src/main/java/pascal/taie/analysis/dataflow/analysis/constprop/ConstantPropagation.java
@@ -22,23 +22,22 @@
 
 package pascal.taie.analysis.dataflow.analysis.constprop;
 
+import pascal.taie.Assignment;
 import pascal.taie.analysis.dataflow.analysis.AbstractDataflowAnalysis;
 import pascal.taie.analysis.graph.cfg.CFG;
 import pascal.taie.config.AnalysisConfig;
 import pascal.taie.ir.IR;
-import pascal.taie.ir.exp.ArithmeticExp;
-import pascal.taie.ir.exp.BinaryExp;
-import pascal.taie.ir.exp.BitwiseExp;
-import pascal.taie.ir.exp.ConditionExp;
-import pascal.taie.ir.exp.Exp;
-import pascal.taie.ir.exp.IntLiteral;
-import pascal.taie.ir.exp.ShiftExp;
-import pascal.taie.ir.exp.Var;
+import pascal.taie.ir.exp.*;
 import pascal.taie.ir.stmt.DefinitionStmt;
 import pascal.taie.ir.stmt.Stmt;
+import pascal.taie.language.classes.JMethod;
 import pascal.taie.language.type.PrimitiveType;
 import pascal.taie.language.type.Type;
 import pascal.taie.util.AnalysisException;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
 
 public class ConstantPropagation extends
         AbstractDataflowAnalysis<Stmt, CPFact> {
@@ -57,31 +56,78 @@ public class ConstantPropagation extends
     @Override
     public CPFact newBoundaryFact(CFG<Stmt> cfg) {
         // TODO - finish me
-        return null;
+        // 将方法的参数设置为NAC，避免直接被优化掉
+        CPFact res = new CPFact();
+        IR ir = cfg.getIR();
+        List<Var> paras = ir.getParams();
+        for (Var para : paras) {
+            res.update(para, Value.getNAC());
+        }
+        return res;
     }
 
     @Override
     public CPFact newInitialFact() {
         // TODO - finish me
-        return null;
+        // 一开始的变量类型全部是undefined，
+        // 这样在meet的时候才ok
+        CPFact res = new CPFact();
+        return res;
     }
 
     @Override
     public void meetInto(CPFact fact, CPFact target) {
         // TODO - finish me
+        Set<Var> keys = fact.keySet();
+        // 只需要遍历fact的key即可，因为对应在target而没有在fact里的key，
+        // value不需要修改
+        for (Var k : keys) {
+            Value val = fact.get(k);
+            Value new_val = meetValue(val, target.get(k));
+            target.update(k, new_val);
+        }
     }
 
     /**
      * Meets two Values.
+     * 对同一个变量的两个值，他们meet的情况下的处理
      */
     public Value meetValue(Value v1, Value v2) {
         // TODO - finish me
-        return null;
+        // 有一个不是常量，则就不是常量
+        if (v1.isNAC() || v2.isNAC()) return Value.getNAC();
+        // 都是常量，相同，则返回常量，否则的话，则不是常量
+        if (v1.isConstant() && v2.isConstant()) {
+            if (v1.equals(v2)) {
+                return v1;
+            } else {
+                return Value.getNAC();
+            }
+        } else if (v1.isUndef() && v2.isConstant()) {
+            return v2;
+        } else if (v1.isConstant() && v2.isUndef()) {
+            return v2;
+        } else {
+            return Value.getUndef();
+        }
     }
 
     @Override
     public boolean transferNode(Stmt stmt, CPFact in, CPFact out) {
         // TODO - finish me
+        // 不考虑不是赋值语句的表达式
+        if (!(stmt instanceof DefinitionStmt<?,?>)) {
+            return false;
+        }
+        Optional<LValue> def = stmt.getDef();
+        assert def.isPresent();
+        LValue def_ = def.get();
+        // 对于赋值类型的语句，只处理左边为Var的类型，
+        // 对于字段存储等类型，不进行处理。
+        if (!(def_ instanceof  Var)) return false;
+        CPFact new_out = new CPFact();
+
+
         return false;
     }
 
