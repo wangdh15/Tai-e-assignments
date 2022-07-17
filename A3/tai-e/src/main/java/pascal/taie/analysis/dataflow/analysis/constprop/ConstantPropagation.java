8cefc565c765863b994d21f6b02f3c2a8170f703
daohwang <1760730301@qq.com>
2022-07-17 21:48:31 +0800

    add constnatPropagation and liveVariable code

--- A3/tai-e/src/main/java/pascal/taie/analysis/dataflow/analysis/constprop/ConstantPropagation.java
+++ A3/tai-e/src/main/java/pascal/taie/analysis/dataflow/analysis/constprop/ConstantPropagation.java
@@ -26,19 +26,16 @@ import pascal.taie.analysis.dataflow.analysis.AbstractDataflowAnalysis;
 import pascal.taie.analysis.graph.cfg.CFG;
 import pascal.taie.config.AnalysisConfig;
 import pascal.taie.ir.IR;
-import pascal.taie.ir.exp.ArithmeticExp;
-import pascal.taie.ir.exp.BinaryExp;
-import pascal.taie.ir.exp.BitwiseExp;
-import pascal.taie.ir.exp.ConditionExp;
-import pascal.taie.ir.exp.Exp;
-import pascal.taie.ir.exp.IntLiteral;
-import pascal.taie.ir.exp.ShiftExp;
-import pascal.taie.ir.exp.Var;
+import pascal.taie.ir.exp.*;
 import pascal.taie.ir.stmt.DefinitionStmt;
 import pascal.taie.ir.stmt.Stmt;
 import pascal.taie.language.type.PrimitiveType;
 import pascal.taie.language.type.Type;
 import pascal.taie.util.AnalysisException;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
 
 public class ConstantPropagation extends
         AbstractDataflowAnalysis<Stmt, CPFact> {
@@ -56,33 +53,96 @@ public class ConstantPropagation extends
 
     @Override
     public CPFact newBoundaryFact(CFG<Stmt> cfg) {
-        // TODO - finish me
-        return null;
+        // 将方法的参数设置为NAC，避免直接被优化掉
+        CPFact res = new CPFact();
+        IR ir = cfg.getIR();
+        List<Var> paras = ir.getParams();
+        for (Var para : paras) {
+            if (canHoldInt(para)) {
+                res.update(para, Value.getNAC());
+            }
+        }
+        return res;
     }
 
     @Override
     public CPFact newInitialFact() {
-        // TODO - finish me
-        return null;
+        // 一开始的变量类型全部是undefined，
+        // 这样在meet的时候才ok
+        return new CPFact();
     }
 
     @Override
     public void meetInto(CPFact fact, CPFact target) {
-        // TODO - finish me
+        Set<Var> keys = fact.keySet();
+        // 只需要遍历fact的key即可，因为对应在target而没有在fact里的key，
+        // value不需要修改
+        for (Var k : keys) {
+            Value val = fact.get(k);
+            Value new_val = meetValue(val, target.get(k));
+            target.update(k, new_val);
+        }
     }
 
     /**
      * Meets two Values.
      */
     public Value meetValue(Value v1, Value v2) {
-        // TODO - finish me
-        return null;
+        // 有一个不是常量，则就不是常量
+        if (v1.isNAC() || v2.isNAC()) return Value.getNAC();
+        // 都是常量，相同，则返回常量，否则的话，则不是常量
+        if (v1.isConstant() && v2.isConstant()) {
+            if (v1.equals(v2)) {
+                return v1;
+            } else {
+                return Value.getNAC();
+            }
+        } else if (v1.isUndef() && v2.isConstant()) {
+            return v2;
+        } else if (v1.isConstant() && v2.isUndef()) {
+            return v1;
+        } else {
+            return Value.getUndef();
+        }
+    }
+
+    private boolean compare(CPFact new_out, CPFact out) {
+        if (new_out.equals(out)) {
+            return false;
+        } else {
+            out.copyFrom(new_out);
+            return true;
+        }
     }
 
     @Override
     public boolean transferNode(Stmt stmt, CPFact in, CPFact out) {
-        // TODO - finish me
-        return false;
+        // 不考虑不是赋值语句的表达式
+        if (!(stmt instanceof DefinitionStmt<?,?>)) {
+            return compare(in, out);
+        }
+        Optional<LValue> def = stmt.getDef();
+        // 当时invoke函数的时候，def为空
+        if (def.isEmpty()) {
+            return compare(in, out);
+        }
+        LValue def_ = def.get();
+        // 对于赋值类型的语句，只处理左边为Var的类型，
+        // 对于字段存储等类型，不进行处理。
+        if (!(def_ instanceof  Var)) {
+            return compare(in, out);
+        }
+        // 只处理int类型的变量
+        if (!canHoldInt((Var)def_)){
+            return compare(in, out);
+        }
+        CPFact new_out = in.copy();
+        // 将对def_的定义进行删除
+        new_out.remove((Var)def_);
+        RValue r_expr = ((DefinitionStmt<?, ?>)stmt).getRValue();
+        new_out.update((Var)def_, evaluate(r_expr, in));
+        // 判断新的out和之前的out是否相同
+        return compare(new_out, out);
     }
 
     /**
@@ -111,7 +171,132 @@ public class ConstantPropagation extends
      * @return the resulting {@link Value}
      */
     public static Value evaluate(Exp exp, CPFact in) {
-        // TODO - finish me
-        return null;
+        Value res = null;
+        boolean flag = true;
+        if (exp instanceof IntLiteral) {
+            // 如果右侧是一个常量表达式的话
+            res = Value.makeConstant(((IntLiteral)exp).getValue());
+        } else if (exp instanceof Var) {
+            // 如果右侧是一个变量的话
+            res = in.get((Var)exp);
+        } else if (exp instanceof ArithmeticExp) {
+            // 算术表达式
+            Var var1 = ((ArithmeticExp) exp).getOperand1();
+            Var var2 = ((ArithmeticExp) exp).getOperand2();
+            ArithmeticExp.Op op = ((ArithmeticExp) exp).getOperator();
+            if (in.get(var2).isConstant() && in.get(var2).getConstant() == 0 && (op == ArithmeticExp.Op.DIV || op == ArithmeticExp.Op.REM)) {
+                res = Value.getUndef();
+            } else if (in.get(var1).isConstant() && in.get(var2).isConstant()) {
+                // 两个都是常量的情况
+                int val = 0;
+                int val1 =  in.get(var1).getConstant();
+                int val2 = in.get(var2).getConstant();
+                if (op == ArithmeticExp.Op.ADD) {
+                    val = val1 + val2;
+                } else if (op == ArithmeticExp.Op.SUB) {
+                    val = val1 - val2;
+                } else if (op == ArithmeticExp.Op.MUL) {
+                    val = val1 * val2;
+                } else if (op == ArithmeticExp.Op.DIV) {
+                    if (val2 == 0) {
+                        res = Value.getUndef();
+                        flag = false;
+                    } else {
+                        val = val1 / val2;
+                    }
+                } else {
+                    if (val2 == 0) {
+                        res = Value.getUndef();
+                        flag = false;
+                    } else {
+                        val = val1 % val2;
+                    }
+                }
+                if (flag) {
+                    res = Value.makeConstant(val);
+                }
+            } else if (in.get(var1).isNAC() || in.get(var2).isNAC()) {
+                // 有一个不是常量
+                res = Value.getNAC();
+            } else {
+                // 其他情况
+                res = Value.getUndef();
+            }
+        } else if (exp instanceof  ConditionExp) {
+            // 条件表达式
+            Var var1 = ((ConditionExp) exp).getOperand1();
+            Var var2 = ((ConditionExp) exp).getOperand2();
+            ConditionExp.Op op = ((ConditionExp) exp).getOperator();
+            if (in.get(var1).isConstant() && in.get(var2).isConstant()) {
+                int val = 0;
+                int val1 = in.get(var1).getConstant();
+                int val2 = in.get(var2).getConstant();
+                if (op == ConditionExp.Op.EQ) {
+                    if (val1 == val2) val = 1;
+                } else if (op == ConditionExp.Op.NE) {
+                    if (val1 != val2) val = 1;
+                } else if (op == ConditionExp.Op.LT){
+                    if (val1 < val2) val = 1;
+                } else if (op == ConditionExp.Op.GT){
+                    if (val1 > val2) val = 1;
+                } else if (op == ConditionExp.Op.LE) {
+                    if (val1 <= val2) val = 1;
+                } else {
+                    if (val1 >= val2) val = 1;
+                }
+                res = Value.makeConstant(val);
+            } else if (in.get(var1).isNAC() || in.get(var2).isNAC()) {
+                res = Value.getNAC();
+            } else {
+                res = Value.getUndef();
+            }
+        } else if (exp instanceof  ShiftExp) {
+            // 移位表达式
+            Var var1 = ((ShiftExp) exp).getOperand1();
+            Var var2 = ((ShiftExp) exp).getOperand2();
+            ShiftExp.Op op = ((ShiftExp) exp).getOperator();
+            if (in.get(var1).isConstant() && in.get(var2).isConstant()) {
+                int val = 0;
+                int val1 = in.get(var1).getConstant();
+                int val2 = in.get(var2).getConstant();
+                if (op == ShiftExp.Op.SHL) {
+                    val = val1 << val2;
+                } else if (op == ShiftExp.Op.SHR) {
+                    val = val1 >> val2;
+                } else {
+                    val = val1 >>> val2;
+                }
+                res = Value.makeConstant(val);
+            } else if (in.get(var1).isNAC() || in.get(var2).isNAC()) {
+                res = Value.getNAC();
+            } else {
+                res = Value.getUndef();
+            }
+        } else if (exp instanceof BitwiseExp) {
+            // 位运算
+            Var var1 = ((BitwiseExp) exp).getOperand1();
+            Var var2 = ((BitwiseExp) exp).getOperand2();
+            BinaryExp.Op op = ((BitwiseExp) exp).getOperator();
+            if (in.get(var1).isConstant() && in.get(var2).isConstant()) {
+                int val = 0;
+                int val1 = in.get(var1).getConstant(), val2 = in.get(var2).getConstant();
+                if (op == BitwiseExp.Op.OR) {
+                    val = val1 | val2;
+                } else if (op == BitwiseExp.Op.AND) {
+                    val =  val1 & val2;
+                } else {
+                    val = val1 ^ val2;
+                }
+                res = Value.makeConstant(val);
+            }  else if (in.get(var1).isNAC() || in.get(var2).isNAC()) {
+                res = Value.getNAC();
+            } else {
+                res = Value.getUndef();
+            }
+        } else {
+            // 其他表达式
+            res = Value.getNAC();
+        }
+        return res;
     }
 }
--- A3/tai-e/src/main/java/pascal/taie/analysis/dataflow/solver/WorkListSolver.java
+++ A3/tai-e/src/main/java/pascal/taie/analysis/dataflow/solver/WorkListSolver.java
@@ -25,6 +25,12 @@ package pascal.taie.analysis.dataflow.solver;
 import pascal.taie.analysis.dataflow.analysis.DataflowAnalysis;
 import pascal.taie.analysis.dataflow.fact.DataflowResult;
 import pascal.taie.analysis.graph.cfg.CFG;
+import pascal.taie.analysis.graph.cfg.Edge;
+
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Set;
 
 class WorkListSolver<Node, Fact> extends Solver<Node, Fact> {
 
@@ -34,11 +40,52 @@ class WorkListSolver<Node, Fact> extends Solver<Node, Fact> {
 
     @Override
     protected void doSolveForward(CFG<Node> cfg, DataflowResult<Node, Fact> result) {
-        // TODO - finish me
+        Queue<Node> qe = new LinkedList<>();
+        Set<Node> vis = new HashSet<>();
+        Node entry = cfg.getEntry();
+        for (Node node : cfg) {
+            qe.add(node);
+            vis.add(node);
+        }
+
+        while (!qe.isEmpty()) {
+            Node node = qe.remove();
+            vis.remove(node);
+            Set<Edge<Node>> in_edges = cfg.getInEdgesOf(node);
+            Fact new_in = analysis.newInitialFact();
+            for (Edge<Node> edge : in_edges) {
+                Node in_node = edge.getSource();
+                analysis.meetInto(result.getOutFact(in_node), new_in);
+            }
+            if (analysis.transferNode(node, new_in, result.getOutFact(node))) {
+                Set<Edge<Node>> out_edges = cfg.getOutEdgesOf(node);
+                for (Edge<Node> edge : out_edges) {
+                    Node out_node = edge.getTarget();
+                    if (!vis.contains(out_node)) {
+                        vis.add(out_node);
+                        qe.add(out_node);
+                    }
+                }
+            }
+        }
     }
 
     @Override
     protected void doSolveBackward(CFG<Node> cfg, DataflowResult<Node, Fact> result) {
-        // TODO - finish me
+        boolean need_continue = true;
+        while (need_continue) {
+            need_continue = false;
+            for (Node node : cfg) {
+                if (cfg.isEntry(node) || cfg.isExit(node))  continue;
+                Fact new_out = analysis.newBoundaryFact(cfg);
+                Set<Edge<Node>> out_edges = cfg.getOutEdgesOf(node);
+                for (Edge<Node> edge : out_edges) {
+                    Node target = edge.getTarget();
+                    analysis.meetInto(result.getInFact(target), new_out);
+                }
+                result.setOutFact(node, new_out);
+                need_continue = analysis.transferNode(node, result.getInFact(node), new_out) || need_continue;
+            }
+        }
     }
 }
